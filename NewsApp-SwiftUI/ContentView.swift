22f6bbac6dc1198c9a87ddf66031656ecf3d7a27
marufkhan09 <maruf1609@gmail.com>
2023-11-17 00:47:43 +0600

    Initial Commit

---
+++ NewsApp-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsApp-SwiftUI
+//
+//  Created by Maruf Khan on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsApp-SwiftUI/NewsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_SwiftUIApp.swift
+//  NewsApp-SwiftUI
+//
+//  Created by Maruf Khan on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
